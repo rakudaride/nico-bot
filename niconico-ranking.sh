e17b04a459e420b27e6d58dd20cf0b070443944b
rakudaride <rakudaride@gmail.com>
2017-03-09 21:24:42 +0900

    Update niconico-ranking.sh

--- niconico-ranking.sh
+++ niconico-ranking.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
-dirname="/home/vagrant/htdocs/n_yobiko/nico-bot/ranking-rss"
+dirname=YOUR_DIR_NAME
 mkdir -p $dirname
 
 filename="${dirname}/hourly-ranking-`date +'%Y%m%d%H%M'`.xml"
 echo "Saveto $filename"
 
-curl -s -o $filename -H "User-Agent: CrawlBot; rakudaride@gmail.com" http://www.nicovideo.jp/ranking/fav/hourly/all?rss=2.0&lang=ja-jp
+curl -s -o $filename -H "User-Agent: CrawlBot; YOUR_EMAIL http://www.nicovideo.jp/ranking/fav/hourly/all?rss=2.0&lang=ja-jp
 
 
